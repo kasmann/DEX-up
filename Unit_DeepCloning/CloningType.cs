022c78b389ccf169483354ea61c711682fbea6a5
Casie <damneda@gmail.com>
2019-12-11 10:31:56 +0200

    deep cloning

---
+++ Unit_DeepCloning/CloningType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Unit_DeepCloning
+{
+    public class Student : ICloneable
+    {
+        public string FullName;
+        public int Ages;
+        public bool FellowshipHolder;
+        public Gender Gender;
+
+        public object Clone()
+        {
+            var stud = MemberwiseClone() as Student;
+            stud.Gender = this.Gender.Clone() as Gender;
+            return stud;
+        }
+    }
+
+    public class Gender : ICloneable
+    {
+        public string Value { get; set; }
+
+        public Gender(string male)
+        {
+            Value = male;
+        }
+        
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
---
+++ Unit_DeepCloning/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Unit_DeepCloning
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var student1 = new Student()
+            {
+                FullName = "Jane Smith", 
+                Ages = 18,
+                Gender = new Gender("female"), 
+                FellowshipHolder = true,
+            };
+            Console.WriteLine($"{student1.FullName} \t {student1.Ages} \t {student1.Gender.Value} \t {student1.FellowshipHolder}");
+
+            var student2 = student1.Clone() as Student;
+            Console.WriteLine($"{student2.FullName} \t {student2.Ages} \t {student2.Gender.Value} \t {student2.FellowshipHolder}");
+            student2.FullName = "John Smith";
+            student2.Gender.Value = "male";
+            Console.WriteLine($"{student2.FullName} \t {student2.Ages} \t {student2.Gender.Value} \t {student2.FellowshipHolder}");
+        }
+    }
+}
