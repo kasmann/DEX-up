2ba05f070a68e9872cca12b5c8d13bf0ec3d07e3
Casie <damneda@gmail.com>
2020-02-28 12:39:09 +0200

    tests

---
+++ TestingTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestingTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
